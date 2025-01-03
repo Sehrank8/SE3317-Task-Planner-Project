5f39c7be3f29e1efe934021b49735c74c0f169ea
Sehrank8 <64368795+Sehrank8@users.noreply.github.com>
2025-01-03 20:50:40 +0300

    sql şifre

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/project";
     private static final String USER = "root";
-    private static final String PASSWORD = "your_password";
+    private static final String PASSWORD = "12345678";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
